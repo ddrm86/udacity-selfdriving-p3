6af51be3e3be48a4ee2d32266ccdfec07464a7d4
David del Río Medina <ddrm86@gmail.com>
2017-01-16 18:00:51 +0100

    Add working test model using all Udacity data
    Former-commit-id: ce5a66595994a8eb74c7e7596b9663489c132b06

--- tests.py
+++ tests.py
@@ -1,39 +1,35 @@
 import pandas as pd
+import pickle
 import numpy as np
 from keras.models import Sequential
-from keras.layers.core import Dense, Activation, Flatten
+from keras.layers import Dense, Dropout, Flatten, Lambda, ELU, Activation
+from keras.layers.convolutional import Convolution2D
 from matplotlib.image import imread
 import json
 import matplotlib.pyplot as plt
 
-dire = '../udacity-data/'
-log = pd.read_csv(dire + 'driving_log.csv')
+train_data = '../udacity-data/train.p'
 width = 320
 height = 160
 depth = 3
-steer_right = log.iloc[55]
-steer_left = log.iloc[1181]
-steer_straight = log.iloc[0]
 
-img_right = imread(dire + steer_right.center)
-img_left = imread(dire + steer_left.center)
-img_straight = imread(dire + steer_straight.center)
+with open(train_data,'rb') as f_train_data:
+    train = pickle.load(f_train_data)
 
-X_train = np.array([img_right, img_left, img_straight])
-y_train = np.array([steer_right.steering, steer_left.steering, steer_straight.steering])
-
+X_train = train['features']
+y_train = train['labels']
 
 model = Sequential()
 model.add(Flatten(input_shape=(height, width, depth)))
-model.add(Dense(128))
+model.add(Dense(512))
 model.add(Activation('relu'))
 model.add(Dense(1))
+model.compile('adam', 'mean_squared_error', ['accuracy'])
 
-model.compile('sgd', 'mean_squared_error', ['accuracy'])
-history = model.fit(X_train, y_train, batch_size=128, nb_epoch=10, validation_split=0.0)
+history = model.fit(X_train, y_train, batch_size=128, nb_epoch=10, validation_split=0.2)
+
 print(history)
-print(y_train)
-print(model.predict(X_train))
+
 model.save_weights("model.h5", True)
 with open('model.json', 'w') as outfile:
     json.dump(model.to_json(), outfile)
